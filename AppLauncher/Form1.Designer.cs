4545840d4d945ed2cb2c808827ecdc30ab603ae2
jevs242 <ghostraidenpm@gmail.com>
2022-08-04 16:53:32 -0400

    Initial commit

---
+++ AppLauncher/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace AppLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_OpenFile = new System.Windows.Forms.Button();
+            this.btn_RunApps = new System.Windows.Forms.Button();
+            this.lbl_ProgramsList = new System.Windows.Forms.Label();
+            this.lbx_ProgramBox = new System.Windows.Forms.ListBox();
+            this.btn_DeleteFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_OpenFile
+            // 
+            this.btn_OpenFile.Location = new System.Drawing.Point(349, 302);
+            this.btn_OpenFile.Name = "btn_OpenFile";
+            this.btn_OpenFile.Size = new System.Drawing.Size(93, 38);
+            this.btn_OpenFile.TabIndex = 0;
+            this.btn_OpenFile.Text = "Open File";
+            this.btn_OpenFile.UseVisualStyleBackColor = true;
+            this.btn_OpenFile.Click += new System.EventHandler(this.btn_OpenFile_Click);
+            // 
+            // btn_RunApps
+            // 
+            this.btn_RunApps.Location = new System.Drawing.Point(349, 346);
+            this.btn_RunApps.Name = "btn_RunApps";
+            this.btn_RunApps.Size = new System.Drawing.Size(93, 38);
+            this.btn_RunApps.TabIndex = 1;
+            this.btn_RunApps.Text = "Run Apps";
+            this.btn_RunApps.UseVisualStyleBackColor = true;
+            this.btn_RunApps.Click += new System.EventHandler(this.btn_RunApps_Click);
+            // 
+            // lbl_ProgramsList
+            // 
+            this.lbl_ProgramsList.AutoSize = true;
+            this.lbl_ProgramsList.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ProgramsList.Location = new System.Drawing.Point(314, 35);
+            this.lbl_ProgramsList.Name = "lbl_ProgramsList";
+            this.lbl_ProgramsList.Size = new System.Drawing.Size(186, 31);
+            this.lbl_ProgramsList.TabIndex = 2;
+            this.lbl_ProgramsList.Text = "Program\'s List";
+            // 
+            // lbx_ProgramBox
+            // 
+            this.lbx_ProgramBox.FormattingEnabled = true;
+            this.lbx_ProgramBox.Location = new System.Drawing.Point(205, 84);
+            this.lbx_ProgramBox.Name = "lbx_ProgramBox";
+            this.lbx_ProgramBox.Size = new System.Drawing.Size(416, 199);
+            this.lbx_ProgramBox.TabIndex = 3;
+            // 
+            // btn_DeleteFile
+            // 
+            this.btn_DeleteFile.Location = new System.Drawing.Point(358, 394);
+            this.btn_DeleteFile.Name = "btn_DeleteFile";
+            this.btn_DeleteFile.Size = new System.Drawing.Size(75, 23);
+            this.btn_DeleteFile.TabIndex = 4;
+            this.btn_DeleteFile.Text = "Delete File";
+            this.btn_DeleteFile.UseVisualStyleBackColor = true;
+            this.btn_DeleteFile.Click += new System.EventHandler(this.btn_DeleteFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_DeleteFile);
+            this.Controls.Add(this.lbx_ProgramBox);
+            this.Controls.Add(this.lbl_ProgramsList);
+            this.Controls.Add(this.btn_RunApps);
+            this.Controls.Add(this.btn_OpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_OpenFile;
+        private System.Windows.Forms.Button btn_RunApps;
+        private System.Windows.Forms.Label lbl_ProgramsList;
+        private System.Windows.Forms.ListBox lbx_ProgramBox;
+        private System.Windows.Forms.Button btn_DeleteFile;
+    }
+}
+
---
+++ AppLauncher/Form1.cs
@@ -0,0 +1,130 @@
+﻿//Jose Velazquez
+//App Launcher
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace AppLauncher
+{
+    public partial class Form1 : Form
+    {
+        //List of program url
+        List<string> urlList = new List<string>();
+        //Path to where will go the file and name of txt
+        string path = "Data\\saved.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreateFile(false);
+            ReadFile();
+        }
+
+        private void btn_OpenFile_Click(object sender, EventArgs e)
+        {
+            string filePath = string.Empty;
+            //Create the file dialog to search the program and get the url
+            using(OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.exe*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+                //For when you put OPEN button in the file Dialog
+                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get URL
+                    filePath = openFileDialog.FileName;
+
+                    Stream fileSteam = openFileDialog.OpenFile();
+                    //Add URL to the list
+                    urlList.Add(filePath.ToString());
+
+                    AddTextBox();
+
+                    WriteFile(filePath);
+                }
+            }
+        }
+
+        private void btn_RunApps_Click(object sender, EventArgs e)
+        {
+            //Open all the programs that are in the list
+            for(int i=0; i < urlList.Count; i++)
+            {
+                Process.Start(urlList[i]);
+            }
+        }
+
+        private void CreateFile(bool delete)
+        {
+            //Condition to know if there some file with the same path
+            //if is to delete file must ignore the first condisional
+            if (!File.Exists(path) || delete)
+            {
+                StreamWriter sw = File.CreateText(path);
+                sw.Close();
+            }
+        }
+
+        private void WriteFile(string newUrl)
+        {
+            //Rewrite the file with the new list
+            TextWriter sw = File.CreateText(path);
+            for (int i = 0; i < urlList.Count; i++)
+            {
+                sw.Write(urlList[i]);
+                sw.Write("\n");
+            }   
+            sw.Close();
+        }
+
+        private void ReadFile()
+        {
+            StreamReader sr = new StreamReader(path);
+            //Puts the file information in filedata
+            string filedata = sr.ReadToEnd();
+
+            int numUrl = 0;
+            string url = string.Empty;
+            
+            //Divide the file string to List through Enter
+            for(int i = 0; i < filedata.Length; i++)
+            {
+                if (filedata[i] == '\n')
+                {
+                    numUrl++;
+                    urlList.Add(url);
+                    url = string.Empty;
+                    continue;
+                }   
+                url += filedata[i];
+            }
+
+            AddTextBox();
+
+            sr.Close();
+        }
+
+        private void AddTextBox()
+        {
+            //Clear and put new information in Box List
+            lbx_ProgramBox.Items.Clear();
+            for (int i = 0; i < urlList.Count; i++)
+            {
+                lbx_ProgramBox.Items.Add(urlList[i]);
+            }
+        }
+
+        private void btn_DeleteFile_Click(object sender, EventArgs e)
+        {
+            //Delete the List and Rewrite the file for delete information
+            urlList.Clear();
+            CreateFile(true);
+            AddTextBox();
+        }
+    }
+}
